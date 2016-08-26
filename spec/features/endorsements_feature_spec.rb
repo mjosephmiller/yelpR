4ca6a22695016a728d323a1674a30a908732cc29
Michael <mjosephmiller.uk@gmail.com>
2016-08-26 12:26:30 +0100

    fixes endorsement feature test

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -4,13 +4,19 @@ feature 'endorsing reviews' do
   before do
     sign_up
     create_restaurant
+    click_link 'Review KFC'
+    fill_in 'Thoughtsandfeelings', with: 'so so'
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
     click_link 'Sign out'
   end
 
   scenario 'a user can endorse a review, which updates the review endorsement count' do
    sign_up
    visit '/restaurants'
+   click_link 'KFC'
    click_link 'Endorse'
+   click_link 'KFC'
    expect(page).to have_content('1 endorsement')
   end
 
