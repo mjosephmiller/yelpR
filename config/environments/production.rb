8f4315ef02799cbf7b7a266d2a0238976fa4917f
Michael <mjosephmiller.uk@gmail.com>
2016-08-27 03:06:17 +0100

    heroku debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,12 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      :s3_region => 'us-east-1'
-
+    storage: :s3,
+    s3_region: ENV.fetch('AWS_REGION'),
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     }
   }
   # Code is not reloaded between requests.
